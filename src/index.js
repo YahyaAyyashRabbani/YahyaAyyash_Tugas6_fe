bffcfa4ea6941bf7f26409fcabe82bea86f28407
YahyaAyyashRabbani <142917111+YahyaAyyashRabbani@users.noreply.github.com>
2025-04-20 19:39:34 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "bulma/css/bulma.css";
+import { base_url } from './utils.js'; // atau require jika pakai CommonJS
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
